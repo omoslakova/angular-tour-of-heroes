7fb8bd41b7498eaf5a6a05f53d7891d490a03592
omoslakova <o.moslakova@yandex.by>
2022-04-16 16:26:08 +0300

    implementation complete

---
+++ src/app/hero.ts
@@ -0,0 +1,5 @@
+export class Hero {
+    id!: number; //When you add an exclamation mark after variable/property name, you’re telling to TypeScript that you’re certain that value is not null or undefined.
+    name: string = '' //Set name property to empty string
+    
+  }
